57323a3699a9c708983f554d3149e20bf8273cf7
Techognito <techognito@KingsThrone.bacon.castle>
2024-07-28 14:37:46 +0200

    updated charcoal mod

--- mods/charcoal/init.lua
+++ mods/charcoal/init.lua
@@ -1,69 +1,95 @@
---adds charcoal craftitem
-minetest.register_craftitem('charcoal:charcoal', {
-	description = 'Charcoal',
-	inventory_image = 'charcoal_charcoal.png',
-	groups = {coal=1},
-	stack_max = 99,
+--[[
+		Minetest-mod "Charcoal", A mod with charcoal lumps and blocks
+		Copyright (C) 2021 J. A. Anders
+
+		This program is free software; you can redistribute it and/or modify
+		it under the terms of the GNU General Public License as published by
+		the Free Software Foundation; version 3 of the License.
+
+		This program is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU General Public License for more details.
+
+		You should have received a copy of the GNU General Public License
+		along with this program; if not, write to the Free Software
+		Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+		MA 02110-1301, USA.
+]]
+
+--
+-- Mod Version 0.1
+--
+
+charcoal = {}
+
+-- Get Translator
+local S = minetest.get_translator("charcoal")
+charcoal.get_translator = S
+local S = charcoal.get_translator
+
+-- Basic Charcoal
+minetest.register_craftitem("charcoal:charcoal", {
+  description = S("Charcoal Lump"),
+  inventory_image = "charcoal_charcoal.png",
 })
 
-minetest.register_craftitem('charcoal:charcoal_block', {
-        description = 'Charcoal Block',
-        inventory_image = 'charcoal_charcoal_block_icon.png',
-        groups = {coal=1},
-        stack_max = 99,
+-- Charcoal Block
+minetest.register_node("charcoal:charcoal_block", {
+  description = S("Charcoal Block"),
+  inventory_image = minetest.inventorycube("charcoal_charcoal_block.png"),
+  tiles = {"charcoal_charcoal_block.png"},
+  groups = {cracky=3, oddly_breakalbe_by_hand=1},
+  sounds = default.node_sound_stone_defaults(),
 })
 
---Initial creation
+
+-- Use Charcoal as fuel
 minetest.register_craft({
-	type = 'cooking',
-	output = 'charcoal:charcoal',
-	recipe = 'group:tree',
-})
-
---Into a block
-minetest.register_craft({
-        output = 'charcoal:charcoal_block',
-        recipe = {
-                    {'charcoal:charcoal', 'charcoal:charcoal', 'charcoal:charcoal'},
-                    {'charcoal:charcoal', 'charcoal:charcoal', 'charcoal:charcoal'},
-                    {'charcoal:charcoal', 'charcoal:charcoal', 'charcoal:charcoal'},
-                }
-})
-
---The block node
-minetest.register_node('charcoal:charcoal_block', {
-	description = 'Charcoal Block',
-	tiles = {'charcoal_charcoal_block.png'},
-	groups = {cracky=1},
-	stack_max = 99,
-})
-
---as fuel
-minetest.register_craft({
-	type = 'fuel',
-	recipe = 'charcoal:charcoal',
-	burntime = 40,
+  type = "fuel",
+  recipe = "charcoal:charcoal",
+  burntime = 30,
 })
 
 minetest.register_craft({
-	type = 'fuel',
-	recipe = 'charcoal:charcoal_block',
-	burntime = 370,
+  type = "fuel",
+  recipe = "charcoal:charcoal_block",
+  burntime = 270,
 })
 
---in torches
+
+
+-- Craft Charcoal
 minetest.register_craft({
-	output = 'default:torch 4',
-	recipe = {
-			{'charcoal:charcoal'},
-			{'default:stick'},
-		}
+  type = "cooking",
+  output = "charcoal:charcoal 2",
+  recipe = "group:tree",
+  cooktime = 8,
 })
 
---reversing block
 minetest.register_craft({
-	output = 'charcoal:charcoal 9',
-	recipe = {
-			{'charcoal:charcoal_block'},
-		 }
+  type = "shaped",
+  output = "charcoal:charcoal_block",
+  recipe = {
+    {"charcoal:charcoal","charcoal:charcoal","charcoal:charcoal"},
+    {"charcoal:charcoal","charcoal:charcoal","charcoal:charcoal"},
+    {"charcoal:charcoal","charcoal:charcoal","charcoal:charcoal"},
+  },
 })
+
+minetest.register_craft({
+  type = "shapeless",
+  output = "charcoal:charcoal 9",
+  recipe = {"charcoal:charcoal_block"}
+})
+
+-- Make torches from charcoal
+minetest.register_craft({
+  type = "shaped",
+  output = "default:torch 2",
+  recipe = {
+    {"charcoal:charcoal"},
+    {"default:stick"},
+  },  
+})
+
